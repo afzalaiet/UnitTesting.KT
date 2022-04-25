1762498c4eb03e471d7a2c7fd2b32992765e790a
Afzal.Ahmad <afzal.ahmad@globallogic.com>
2022-04-26 02:38:18 +0530

    Added few description.

--- UnitTesting.KT/Logic/Logic_Dependency.cs
+++ UnitTesting.KT/Logic/Logic_Dependency.cs
@@ -10,7 +10,11 @@ namespace UnitTesting.KT.API.Logic
         {
             _employeeRepository = employeeRepository;
         }
-
+        /// <summary>
+        /// AC: As a user, I want to add one new employee into the system, provided it has non empty name.
+        /// </summary>
+        /// <param name="employee">Employee object.</param>
+        /// <returns></returns>
         public bool CreateEmployee(Employee employee)
         {
             if (!string.IsNullOrWhiteSpace(employee.Name))
--- UnitTesting.KT/Logic/Logic_Simple.cs
+++ UnitTesting.KT/Logic/Logic_Simple.cs
@@ -1,7 +1,12 @@
 ﻿namespace UnitTesting.KT.API.Logic
 {
     public class SimpleLogic : ISimpleLogic
-    {        
+    { 
+        /// <summary>
+        /// AC: As a user, I want get the bonus of any employee calculated basis on 1) equal to the salarey if age is less than 50 2) double to the salary if age is equal or greater than 50.
+        /// </summary>
+        /// <param name="employee"></param>
+        /// <returns></returns>
         public int CalculateEmployeeBonas(Employee employee)
         {
             if (employee.Age >= 50)
